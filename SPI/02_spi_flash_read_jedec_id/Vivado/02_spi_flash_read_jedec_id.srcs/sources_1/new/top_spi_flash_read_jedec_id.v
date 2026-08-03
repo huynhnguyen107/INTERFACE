c485dcac84b56ee72a19accbb03f13f1d5fcae20
nvhuynh107@gmail.com <nvhuynh107@gmail.com>
2026-08-03 16:18:46 -0400

    sim done but some errors with hadware connectin pins

---
+++ SPI/02_spi_flash_read_jedec_id/Vivado/02_spi_flash_read_jedec_id.srcs/sources_1/new/top_spi_flash_read_jedec_id.v
@@ -0,0 +1,87 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: nvhuynh107@gmail.com
+// Engineer: Van-Huynh Nguyen
+// 
+// Create Date: 08/03/2026 11:39:15 AM
+// Design Name: top_spi_flash_read_jedec_id
+// Module Name: top_spi_flash_read_jedec_id
+// Project Name: spi_flash_read_jedec_id
+// Target Devices: KRIA(KRC260)+flash W25Q64 SPI Flash 3.3V
+// Tool Versions: Vivado
+// Description: KRIA uses spi to read flas jedec id (W25Q64 SPI Flash 3.3V.)
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_spi_flash_read_jedec_id(
+	input clk_25mhz,
+	input rst_n,
+	input start,
+	//SPI interface
+	output spi_clk, //1s
+	output spi_mosi, //master out
+	output spi_cs_n, //chip selec n
+	input spi_miso, //master in
+	//status
+	output reg done_led, //done_led
+	output reg busy_led //busy_led
+    );
+	//signal defination
+	wire clk_100mhz;
+	wire done;
+	wire busy;
+	wire [4*8-1: 0] data_in;
+	wire [4*8-1: 0] data_out;
+	
+	//clock100mhz
+    clk_wiz_0 clk_wiz_0
+	   (
+		// Clock out ports
+		.clk_100mhz(clk_100mhz),     // output clk_100mhz
+	   // Clock in ports
+		.clk_25mhz(clk_25mhz)      // input clk_25mhz
+	);
+	//spi_flash_read_jedec_id
+	spi_flash_read_jedec_id #(50000000,4) spi_flash_read_jedec_id(clk_100mhz, rst_n, start, data_in, done, busy, data_out, spi_clk, spi_mosi, spi_cs_n, spi_miso);
+	//out leds
+	always @(posedge clk_100mhz) begin
+		if (!rst_n) begin
+			done_led <=0;
+			busy_led <=0;
+		end
+		else begin
+			if (done)
+				done_led <=1;
+			busy_led <= busy;
+		end
+	end
+	//vio for data_in
+	vio_0 vio_0 (
+	  .clk(clk_100mhz),                // input wire clk
+	  .probe_out0(data_in)  // output wire [31 : 0] probe_out0
+	);
+	//ila
+	ila_0 ila_0 (
+	.clk(clk_100mhz), // input wire clk
+
+
+	.probe0(rst_n), // input wire [0:0]  probe0  
+	.probe1(start), // input wire [0:0]  probe1 
+	.probe2(spi_clk), // input wire [0:0]  probe2 
+	.probe3(spi_mosi), // input wire [0:0]  probe3 
+	.probe4(spi_cs_n), // input wire [0:0]  probe4 
+	.probe5(spi_miso), // input wire [0:0]  probe5 
+	.probe6(done_led), // input wire [0:0]  probe6 
+	.probe7(busy_led), // input wire [0:0]  probe7 
+	.probe8(data_in), // input wire [31:0]  probe8 
+	.probe9(data_out) // input wire [31:0]  probe9
+);
+
+endmodule
