707fca44ca03c8855c3149859425c36df3dd3912
nvhuynh107@gmail.com <nvhuynh107@gmail.com>
2026-08-20 12:05:51 -0400

    01_i2c_bmp280_read_id sim created

---
+++ I2C/01_i2c_bmp280_read_id/Vivado/i2c_bmp280_read_id/i2c_bmp280_read_id.srcs/sim_1/new/tb.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: nvhuynh107@gmail.com
+// Engineer: Van-Huynh Nguyen 
+// 
+// Create Date: 08/20/2026 11:32:04 AM
+// Design Name: tb
+// Module Name: tb
+// Project Name: i2c_bmp280_read_id
+// Target Devices: KRIA+BMP280
+// Tool Versions: Vivado 2022
+// Description: KRIA read BMP280 ID using I2C
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module tb ();
+	reg clk;
+	reg rst_n;
+	reg start;
+	wire done;
+	wire busy;
+	wire ack_err;
+	wire [7:0] chip_id;
+	wire scl;
+	wire sda;
+	reg in_scl;
+	reg in_sda;
+	reg write_en;
+	//call instance
+	i2c_bmp280_read_id #(31'd5000000, 7'h76) i2c_bmp280_read_id (clk, rst_n, start, done,
+									busy, ack_err, chip_id, scl, sda);
+	// create rst and initial others
+	initial begin
+		clk=0;
+		rst_n=1;
+		start=0;
+		write_en=0;
+		in_scl=0;
+		in_sda=0;
+	#50 rst_n=0;
+	end
+	// create clock
+	always #0.5 clk = !clk;
+	//create others
+	initial begin
+		wait(!rst_n)
+		@(posedge clk) begin
+			start <=1;
+			write_en <=0;
+			in_scl <=0;
+			in_sda <=0;
+		end
+		@(posedge clk) begin
+			start <=0;
+			write_en <=0;
+			in_scl <=0;
+			in_sda <=0;
+		end
+		
+	end
+	assign scl = write_en ? in_scl : 1'bz;
+	assign sda = write_en ? in_sda : 1'bz;
+endmodule
---
+++ I2C/01_i2c_bmp280_read_id/Vivado/i2c_bmp280_read_id/i2c_bmp280_read_id.srcs/sources_1/new/i2c_bmp280_read_id.v
@@ -0,0 +1,257 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: nvhuynh107@gmail.com
+// Engineer: Van-Huynh Nguyen
+// 
+// Create Date: 08/20/2026 08:42:40 AM
+// Design Name: i2c_bmp280_read_id
+// Module Name: i2c_bmp280_read_id
+// Project Name: i2c_bmp280_read_id
+// Target Devices: KRIA+BMP280
+// Tool Versions: Vivado 2022
+// Description: KRIA read BMP280 ID using I2C
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2c_bmp280_read_id
+#(
+	parameter [31:0] CLK_DIV =5000000,
+	parameter [6:0] BMP280_ADD =7'h76
+)
+(
+	//in
+	input clk,
+	input rst_n,
+	input start,
+	//out
+	output done,
+	output busy,
+	output reg ack_err,
+	output reg [7:0] chip_id,
+	//inout
+	inout scl,
+	inout sda
+);
+	//constants
+	localparam [7:0] BMP280_ADD_W= {CLK_DIV,1'b0};
+	localparam [7:0] BMP280_ADD_R= {CLK_DIV,1'b1};
+	localparam [7:0] BMP280_REG_ID= 8'hD0;
+	//finite state machine
+	localparam [3:0] 
+		IDLE= 4'd0, 
+		START= 4'd1, 
+		IC_ADD_W= 4'd2, 
+		ACK_IC_ADD_W= 4'd3, 
+		REG_ADD= 4'd4, 
+		ACK_REG_ADD= 4'd5, 
+		RE_START= 4'd6, 
+		IC_ADD_R= 4'd7, 
+		ACK_IC_ADD_R= 4'd8, 
+		READ_ID= 4'd9, 
+		NACK_READ_ID= 4'd10, 
+		STOP= 4'd11;
+	//reg and wire
+	reg [3:0] state;
+	reg [3:0] next_state;
+	reg d_start;
+	reg phase;
+	wire rasing_start;
+	reg [1:0] cnt_re_start;
+	reg [3:0] cnt_bit;
+	reg r_scl;
+	reg r_sda;
+	//state transition and rasing edge start, phase
+	always @(posedge clk) begin
+		if (!rst_n) begin
+			d_start <=0;
+			state <=0;
+		end
+		else begin
+			d_start <=start;
+			state <=next_state;
+		end
+	end
+	//rasing_start
+	assign rasing_start = (!d_start) & start;
+	//FSM
+	always @(*) begin
+		case(state)
+			IDLE: next_state = rasing_start ? START : IDLE;
+			START: next_state = IC_ADD_W;
+			IC_ADD_W: next_state = cnt_bit==7 ? ACK_IC_ADD_W: IC_ADD_W;
+			ACK_IC_ADD_W: next_state = ack_err ? STOP: REG_ADD;
+			REG_ADD: next_state = cnt_bit==7 ? ACK_REG_ADD: REG_ADD;
+			ACK_REG_ADD: next_state = ack_err ? STOP: RE_START;
+			RE_START: next_state = cnt_re_start==1 ? IC_ADD_R : RE_START;
+			IC_ADD_R: next_state = cnt_bit==7 ? ACK_IC_ADD_R: IC_ADD_R;
+			ACK_IC_ADD_R: next_state = ack_err ? STOP: READ_ID;
+			READ_ID: next_state = cnt_bit==7 ? NACK_READ_ID: READ_ID;
+			NACK_READ_ID: next_state = STOP;
+			STOP: next_state = IDLE;
+			default: next_state = IDLE;
+		endcase
+	end
+	//transmit data
+	always @(posedge clk) begin
+		if (!rst_n) begin
+			phase <=0 ;
+			ack_err <=0 ;
+			r_scl <=0 ;
+			r_sda <=0 ;
+			cnt_re_start <=0 ;
+			cnt_bit <=0 ;
+			chip_id <=0 ;
+		end
+		else begin
+			if (state == IDLE) begin
+				r_scl <=1 ;
+				r_sda <=1 ;
+			end
+			else if (state== START) begin
+				//r_scl=1 & r_sda from 1 to 0
+				r_scl <=1 ;
+				r_sda <=0 ;
+			end
+			else if (state== IC_ADD_W) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+				end
+				//update
+				else begin
+					r_scl <=0;
+					r_sda <=BMP280_ADD_W[7-cnt_bit];
+					cnt_bit <= (cnt_bit==7) ? 0 : cnt_bit +1 ;
+				end
+			end
+			else if (state== ACK_IC_ADD_W) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+					if (sda)
+						ack_err <= 1;
+					else
+						ack_err <= 0;
+				end
+				//update
+				else begin
+					r_scl <=0;
+				end
+			end
+			else if (state== REG_ADD) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+				end
+				//update
+				else begin
+					r_scl <=0;
+					r_sda <=BMP280_ADD_R[7-cnt_bit];
+					cnt_bit <= (cnt_bit==7) ? 0 : cnt_bit +1 ;
+				end
+			end
+			else if (state== ACK_REG_ADD) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+					if (sda)
+						ack_err <= 1;
+					else
+						ack_err <= 0;
+				end
+				//update
+				else begin
+					r_scl <=0;
+				end
+			end
+			else if (state== RE_START) begin
+				cnt_re_start <= (cnt_re_start==1) ? 0 : cnt_re_start +1;
+				if (cnt_re_start ==0) begin
+					r_scl <=1;
+					r_sda <=1;
+				end
+				else if (cnt_re_start ==1) begin
+					r_scl <=1;
+					r_sda <=0;
+				end
+			end
+			else if (state== IC_ADD_R) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+				end
+				//update
+				else begin
+					r_scl <=0;
+					r_sda <=BMP280_ADD_R[7-cnt_bit];
+					cnt_bit <= (cnt_bit==7) ? 0 : cnt_bit +1 ;
+				end
+			end
+			else if (state== ACK_IC_ADD_R) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+					if (sda)
+						ack_err <= 1;
+					else
+						ack_err <= 0;
+				end
+				//update
+				else begin
+					r_scl <=0;
+				end
+			end
+			else if (state== READ_ID) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+				end
+				//update
+				else begin
+					r_scl <=0;
+					chip_id[7-cnt_bit] <=sda;
+					cnt_bit <= (cnt_bit==7) ? 0 : cnt_bit +1 ;
+				end
+			end
+			else if (state== NACK_READ_ID) begin
+				phase <= !phase ;
+				//sample
+				if (phase) begin
+					r_scl <=1 ;
+				end
+				//update
+				else begin
+					r_scl <=0;
+					r_sda <=0;
+				end
+			end
+			else if (state== STOP) begin
+				phase <= !phase ;
+				//sample(r_scl=1, sda from 0 to 1)
+				if (phase) begin
+					r_scl <=1 ;
+					r_sda <=1;
+				end
+				//update
+				else begin
+					r_scl <=1;
+					r_sda <=0;
+				end
+			end
+		end
+	end
+endmodule
